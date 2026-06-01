0ee0cce32d92b4bd727176c4cf5ce94a560b9a74
farman.arefin <farman.arefin1@outlook.com>
2026-06-01 13:14:31 +0600

    Remove default boilerplate files and views

--- routers/router.go
+++ routers/router.go
@@ -1,10 +1,2 @@
 package routers
 
-import (
-	"expense-tracker-api/controllers"
-	beego "github.com/beego/beego/v2/server/web"
-)
-
-func init() {
-    beego.Router("/", &controllers.MainController{})
-}
